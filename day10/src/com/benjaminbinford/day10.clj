cdc4b53f0342877ecd825e8434d5db33be2c7482
benbinford <ben.binford@gmail.com>
2024-12-10 20:43:32 -0500

    day10

--- day10/src/com/benjaminbinford/day10.clj
+++ day10/src/com/benjaminbinford/day10.clj
@@ -40,66 +40,64 @@
 
 (find-trailheads sample)
 ;;=> ([2 0] [4 0] [4 2] [6 4] [2 5] [5 5] [0 6] [6 6] [1 7])
-
 (def find-path
-  (memoize
-   (fn [input [i j]]
-     (let [c (cref input i j)
-           next-c (next-path c)]
-       (if (= \9 c)
-         (do ;(println "found summit [i j]" [i j])
-           #{[i j]})
-         (reduce #(into %1 %2) #{}
-                 (for [[di dj] [[0 -1] [1 0] [0 1] [-1 0]]
-                       :let [ni (+ i di)
-                             nj (+ j dj)]
-                       :when (and (>= ni 0) (< ni (count (first input)))
-                                  (>= nj 0) (< nj (count input))
-                                  (= (cref input ni nj) next-c))]
-                   (do ;(println "considering [ni nj] nexst-c" [ni nj] next-c)
-                     (find-path input [ni nj])))))))))
+  (do
+    (fn [input [i j] creator]
+      (let [c (cref input i j)
+            next-c (next-path c)]
+        (if (= \9 c)
+          (do ;(println "found summit [i j]" [i j])
+            (creator [i j]))
+          (reduce #(into %1 %2) (creator)
+                  (for [[di dj] [[0 -1] [1 0] [0 1] [-1 0]]
+                        :let [ni (+ i di)
+                              nj (+ j dj)]
+                        :when (and (>= ni 0) (< ni (count (first input)))
+                                   (>= nj 0) (< nj (count input))
+                                   (= (cref input ni nj) next-c))]
+                    (do ;(println "considering [ni nj] nexst-c" [ni nj] next-c)
+                      (find-path input [ni nj] creator)))))))))
 
-(defn score [input]
+(defn score [input creator]
   (reduce +
           (for [[i j] (find-trailheads input)]
-            (count (find-path input [i j])))))
+            (count (find-path input [i j] creator)))))
 
-(find-path sample [2 0])
+(find-path sample [2 0] hash-set)
+;;=> #{[4 3] [1 0] [5 4] [0 3] [4 5]}
+;;=> Execution error (ArityException) at com.benjaminbinford.day10/fn (REPL:52).
+;;   Wrong number of args (0) passed to: clojure.core/set
+;;   
+;;=> [[4 3]
+;;    [5 4]
+;;    [4 5]
+;;    [1 0]
+;;    [0 3]
+;;    [4 3]
+;;    [5 4]
+;;    [4 5]
+;;    [1 0]
+;;    [0 3]
+;;    [4 3]
+;;    [5 4]
+;;    [4 5]
+;;    [1 0]
+;;    [0 3]
+;;    [4 3]
+;;    [5 4]
+;;    [4 5]
+;;    [1 0]
+;;    [0 3]]
 ;;=> #{[4 3] [1 0] [5 4] [0 3] [4 5]}
 ;;=> #{#{#{#{#{#{#{#{#{#{}}}}}}}}}}
-(score sample)
+(score sample hash-set)
 ;;=> 36
 ;;=> 36
 ;;=> 12
-(score input)
+(score input hash-set)
 ;;=> 587
 
 
-
-(def find-path2
-  (memoize
-   (fn [input [i j]]
-     (let [c (cref input i j)
-           next-c (next-path c)]
-       (if (= \9 c)
-         (do ;(println "found summit [i j]" [i j])
-           [[i j]])
-         (reduce #(into %1 %2) []
-                 (for [[di dj] [[0 -1] [1 0] [0 1] [-1 0]]
-                       :let [ni (+ i di)
-                             nj (+ j dj)]
-                       :when (and (>= ni 0) (< ni (count (first input)))
-                                  (>= nj 0) (< nj (count input))
-                                  (= (cref input ni nj) next-c))]
-                   (do ;(println "considering [ni nj] nexst-c" [ni nj] next-c)
-                     (find-path2 input [ni nj])))))))))
-
-
-
-(defn score2 [input]
-  (reduce +
-          (for [[i j] (find-trailheads input)]
-            (count (find-path2 input [i j])))))
 
 (def sample2 (str/split-lines
               ".....0.
@@ -111,15 +109,15 @@
 ..9...."))
 
 
-(find-path2 sample2 [5 0])
+(find-path sample2 [5 0] vector)
+;;=> [[2 6] [2 6] [2 6]]
 ;;=> [[2 6] [2 6] [2 6]]
 ;;=> [[2 6]]
 ;;=> [[2 6]]
 ;;=> [[4 3] [1 0] [5 4] [0 3] [4 5] [4 3] [1 0] [5 4] [0 3] [4 5]]
 ;;=> #{[4 3] [1 0] [5 4] [0 3] [4 5]}
 
-(score2 sample)
+(score sample vector)
 ;;=> 81
-;;=> 51
-(score2 input)
+(score input vector)
 ;;=> 1340

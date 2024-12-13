344ca553981c9c249a2f3b195ecdb7c86a5bf6de
benbinford <ben.binford@gmail.com>
2024-12-13 15:51:16 -0500

    day 13 pt 2

--- day13/src/com/benjaminbinford/day13.clj
+++ day13/src/com/benjaminbinford/day13.clj
@@ -80,3 +80,87 @@ sample
 
 (find-costs input)
 ;;=> 29201
+
+; Button A: X+94, Y+34
+; Button B: X+22, Y+67
+; Prize: X=8400, Y=5400
+; a*m + b *n = x
+; a*o + b*p = y
+; m = 94, n = 22, x = 8400
+; o = 34, p = 67, y = 5400
+; a = (yn - px) / (on - mp) where on - mp != 0
+; b = (ym - ox) / (pm - no) where pm - no != 0
+(defn solveA [x y m n o p]
+  (let [denominator (- (* o n) (* m p))]
+    (if (zero? denominator)
+      nil
+      (let [a (/ (- (* y n) (* p x)) denominator)]
+        (if (ratio? a)
+          nil
+          a)))))
+
+(solveA 8400 5400 94 22 34 67)
+;;=> 80
+
+(defn solveB [x y m n o p]
+  (let [denominator (- (* p m) (* n o))]
+    (if (zero? denominator)
+      nil
+      (let [b (/ (- (* y m) (* o x)) denominator)]
+        (if  (ratio? b)
+          nil
+          b)))))
+
+(solveB 8400 5400 94 22 34 67)
+;;=> 40
+
+
+(solveA 10000000008400 10000000005400 94 22 34 67)
+;;=> nil
+(solveB 10000000008400 10000000005400 94 22 34 67)
+;;=> nil
+
+(solveA 10000000012748 10000000012176 26 67 66 21)
+;;=> 118679050709
+
+(solveB 10000000012748 10000000012176 26 67 66 21)
+;;=> 103199174542
+
+
+
+
+(defn find-cost2 [{:keys [a b prize]}]
+  (println a b prize)
+  (let [x (first prize)
+        y (second prize)
+        m (first a)
+        n (first b)
+        o (second a)
+        p (second b)
+
+        a (solveA x y m n o p)
+        b (solveB x y m n o p)]
+    (if (or (nil? a) (nil? b))
+      0
+      (+ (* 3 a) b))))
+
+
+(defn find-costs2 [records]
+  (reduce + (map find-cost2 records)))
+
+
+
+(find-cost2 {:a [94 34], :b [22 67], :prize [8400 5400]})
+
+
+(find-costs2 sample)
+;;=> 480
+
+(find-costs2 input)
+;;=> 29201
+
+(defn update-prize [[x y]]
+  [(+ x 10000000000000) (+ y 10000000000000)])
+
+(find-costs2 (map #(update % :prize update-prize) input))
+;;=> 104140871044942

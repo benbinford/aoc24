f7f02e505bb54b2b6f49d7a63f16d22df951721b
benbinford <ben.binford@gmail.com>
2025-01-01 15:16:42 -0500

    day23 pt2

--- day22/src/com/benjaminbinford/day22.clj
+++ day22/src/com/benjaminbinford/day22.clj
@@ -60,7 +60,7 @@
 
 
 (def possible-diffs [9 8 7 6 5 4 3 2 1 0 -1 -2 -3 -4 -5 -6 -7 -8 -9])
-(def possible-seqs (combo/selections possible-diffs 4))
+(def possible-seqs v)
 
 (defn prices [initial mx]
   (take mx (map #(mod % 10) (evolve-secrets initial))))
--- day23/src/com/benjaminbinford/day23.clj
+++ day23/src/com/benjaminbinford/day23.clj
@@ -1,5 +1,6 @@
 (ns com.benjaminbinford.day23
-  (:require [clojure.string :as str])
+  (:require [clojure.string :as str]
+            [clojure.math.combinatorics :as combo])
   (:gen-class))
 
 (defn assoc-list [m k v]
@@ -120,3 +121,65 @@
 ;;     #{"it" "gu" "gi"}
 ;;     #{"ht" "hy" "cd"}
 ;;     ...}
+
+
+
+
+
+(find-all-connected-triplets input identity)
+
+
+(defn all-connected? [m cs]
+  (when (every? (fn [[c1 c2]]
+                  (let [c1-edges (get m c1 #{})
+                        c2-edges (get m c2 #{})]
+                    (and (c1-edges c2) (c2-edges c1))))
+                (combo/combinations cs 2))
+    cs))
+
+(defn has-n-connections? [m n [_ cs]]
+
+  (let [edges (combo/combinations cs n)]
+    ;; for each partition, 
+    (some (partial all-connected? m) edges)))
+
+(has-n-connections? sample 3 ["ka" (get sample "ka")])
+
+(defn best-connection [m [c1 cs]]
+  (loop [n (count cs)]
+    (if-let [connections (has-n-connections? m n [c1 cs])]
+      (concat [c1] connections)
+      (recur (dec n)))))
+
+
+(best-connection sample ["ka" (get sample "ka")])
+;;=> ("co" "de" "ta")
+
+
+
+(defn find-all-connected [m]
+  (str/join "," (sort (first (sort-by count > (map (partial best-connection m) m))))))
+
+(find-all-connected input)
+;;=> "bs,cf,cn,gb,gk,jf,mp,qk,qo,st,ti,uc,xw"
+
+(str/join "," (sort (into [] (:computers (first (sort-by :edges  > (find-all-connected sample)))))))
+;;=> "aq,tb,ub,vc,wq"
+
+(find-all-connected sample)
+;;=> [{:computers #{"cg" "tb" "de" "aq" "yn"}, :edges #{nil #{"aq" "yn"}}}
+;;    {:computers #{"wq" "tb" "vc" "aq" "ub"}, :edges #{nil #{"vc" "aq"} #{"vc" "ub"} #{"tb" "vc"}}}
+;;    {:computers #{"kh" "tc" "qp" "ta" "ub"}, :edges #{nil #{"qp" "ub"}}}
+;;    {:computers #{"td" "yn" "tc" "wh" "qp"}, :edges #{nil #{"yn" "wh"} #{"tc" "wh"} #{"wh" "qp"}}}
+;;    {:computers #{"cg" "wq" "tb" "vc" "ka"}, :edges #{nil #{"wq" "vc"}}}
+;;    {:computers #{"wq" "tb" "vc" "aq" "ub"}, :edges #{nil #{"wq" "ub"} #{"wq" "tb"} #{"wq" "aq"}}}
+;;    {:computers #{"co" "de" "tc" "ka" "ta"}, :edges #{nil #{"de" "ta"} #{"ka" "ta"} #{"de" "ka"}}}
+;;    {:computers #{"cg" "co" "de" "ka" "ta"}, :edges #{nil #{"co" "ta"} #{"co" "ka"} #{"ka" "ta"}}}
+;;    {:computers #{"cg" "wq" "vc" "aq" "yn"}, :edges #{nil #{"cg" "yn"} #{"wq" "vc"}}}
+;;    {:computers #{"cg" "td" "aq" "yn" "wh"}, :edges #{nil #{"cg" "aq"} #{"td" "wh"}}}
+;;    {:computers #{"kh" "td" "co" "tc" "wh"}, :edges #{nil #{"td" "wh"}}}
+;;    {:computers #{"td" "yn" "tc" "wh" "qp"}, :edges #{nil #{"td" "yn"} #{"td" "tc"} #{"td" "qp"}}}
+;;    {:computers #{"tb" "co" "de" "ka" "ta"}, :edges #{nil #{"de" "ta"} #{"co" "de"} #{"co" "ta"}}}
+;;    {:computers #{"kh" "td" "wh" "qp" "ub"}, :edges #{nil #{"td" "wh"} #{"kh" "ub"}}}
+;;    {:computers #{"kh" "co" "de" "ka" "ta"}, :edges #{nil #{"co" "de"} #{"co" "ka"} #{"de" "ka"}}}
+;;    {:computers #{"wq" "kh" "vc" "qp" "ub"}, :edges #{nil #{"kh" "qp"} #{"wq" "vc"}}}]
